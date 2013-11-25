41d417fc1d4bb7fa480fc0d9db70e380e52f8a34
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-11-25 10:27:53 +0000

    [DIY] solution

--- lib/full_text_search.rb
+++ lib/full_text_search.rb
@@ -1,6 +1,27 @@
 class FullText
 
-  
+  def self.datasource(value = nil)
+    return ( @datasource || [] ) unless value
+    @datasource = value
+  end
+
+  def self.search(&block)
+    new.search &block
+  end
+
+  def initialize(datasource = nil)
+    @datasource = datasource || FullText.datasource
+  end
+
+  def search(&proc)
+    proc.call(self)
+    @datasource.select { |val| val[:name] =~ Regexp.new(text, 'i') }
+  end
+
+  def text(value = nil)
+    return @text || "what" unless value
+    @text = value 
+  end
 
 end
 
